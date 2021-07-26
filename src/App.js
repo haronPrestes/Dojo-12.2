8415d233b39218ae649fdaf783a9bddfc09832b7
Haron Prestes <haronprestes1303@gmail.com>
2021-07-26 18:42:50 -0300

    Trocando o piloto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <form>
+        <fieldset>
+          <label>
+            Nome:
+            <input type="text" id="name" name="name" maxLength="40"/>
+          </label>
+        </fieldset>
+      </form>
     </div>
   );
 }
